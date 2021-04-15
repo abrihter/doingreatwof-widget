270950cd923131ac54f419a283d047444b5aa8bd
bojan <bojan.markovic.freelance@gmail.com>
2021-04-15 03:37:17 +0200

    Updates

---
+++ precache-manifest.fac81e6affaab5346736fb0cd86fdb1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b6baa39bab1dfbdf783edb9f6e7582",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7693064a7a77c1738b83",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "7dd47395b42dbbd44e4d",
+    "url": "./static/js/2.229f04aa.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.229f04aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7693064a7a77c1738b83",
+    "url": "./static/js/main.2d4751e2.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.2d4751e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b05e3f35ddbb6711fc6",
+    "url": "./static/js/runtime-main.c20663cd.js"
+  }
+]);
