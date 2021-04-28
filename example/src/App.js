7069da2300d97b9acd1fe14e162a5fc55d254191
bojan <bojan.markovic.freelance@gmail.com>
2021-04-28 14:57:14 +0200

    Update example

--- example/src/App.js
+++ example/src/App.js
@@ -4,7 +4,15 @@ import DoinGreatWOF from 'doingreatwof-widget'
 import 'doingreatwof-widget/dist/index.css'
 
 const App = () => {
-  return <DoinGreatWOF />
+  return (
+    <DoinGreatWOF
+      dg_key="{your-key}"
+      dg_type="grid"
+      dg_count="6"
+      dg_rating="1"
+      dg_border_color="#00c853"
+    />
+  );
 }
 
 export default App
